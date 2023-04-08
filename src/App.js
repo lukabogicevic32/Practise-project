2b02fd2a94d8c252e6bb5ce249417233f0693d1f
lukaa33 <lukabogicevic06@gmail.com>
2023-04-08 13:40:43 +0200

    Practise is done
    Finished practise project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import User from './components/Users/User';
+import UsersList from './components/Users/UsersList';
 
 
 function App() {
+  const  [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+     return (
+       [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString() },]
+     );
+    });
+  };
   return (
     <div>
-
+     <User onAddUser={addUserHandler}/>
+     <UsersList users={usersList} />
     </div>
   );
 }
